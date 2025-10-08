5aa6eec7c03e8e1d9780d75f92764c1203413f45
siaheguelable <siaheguelable@gmail.com>
2025-10-08 20:49:46 +0200

    still  making changes

--- Backend/swagger.js
+++ Backend/swagger.js
@@ -1,20 +1,19 @@
 const swaggerAutogen = require("swagger-autogen")();
 
-const isProduction = process.env.NODE_ENV === "production";
-const renderHost = process.env.RENDER_HOST || "car-rental-si5p.onrender.com"; // Replace with your actual Render domain
-
 const doc = {
   info: {
     title: "My API",
     description: "API documentation generated with swagger-autogen",
     version: "1.0.0",
   },
-  host: isProduction ? renderHost : "localhost:30000",
-  schemes: isProduction ? ["https"] : ["http"],
+  //the link is
+  host: process.env.RENDER_HOST || "localhost:30000",
+  schemes: process.env.RENDER_SCHEME ? [process.env.RENDER_SCHEME] : ["http"],
   basePath: "/",
 };
 
 const outputFile = "./swagger-output.json";
+// Scan both server.js and routes/user.js!
 const endpointsFiles = ["./server.js"];
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
